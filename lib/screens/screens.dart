537f2d84d16781bbd94ec9697ac2690b1a4763a4
Muhammad Syahrul <after27minutes@gmail.com>
2020-09-18 22:19:44 +0700

    add some basic routes

---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'profile_screen.dart';
+export 'home_screen.dart';
+export 'notifications_screen.dart';
+export 'test_screen.dart';
+export 'auth_screen.dart';
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'bottom_navigation_bar.dart';
+export 'home_widget.dart';
+export 'profile_widget.dart';
+export 'test_card.dart';
